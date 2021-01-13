79ad1b1941a59166aa2b107ff856b15e820cfb88
Athul Mohan <athulmohan@Riafys-MBP.domain.name>
2021-01-13 16:44:00 +0530

    done

--- contactusform/views.py
+++ contactusform/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import redirect,render
-from .models import Contactusform
+from .models import Contactusform,User
 from django.views.generic import View
 from django.http import HttpResponse
 from .forms import Contactform
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponseServerError
 from . import forms
 
 # Create your views here.
@@ -28,7 +28,7 @@ def contactusform_view(request):
 
 
 def database_view(request):
-    form = Contactusform.objects.all()
+    form = User.objects.all()
     return render(request,'contactusform/database.html', {'form': form})
 
 def redirect_view(request):
@@ -36,9 +36,10 @@ def redirect_view(request):
     return response
     
 def saveuser_view(request):
-
+    if request.method!="POST":
+        return HttpResponseServerError()
     if request.method=="POST":
-        contactusform=Contactusform()
+        contactusform=User()
         name = request.POST.get('name')
         email = request.POST.get('email')
         phone_no = request.POST.get('phone_no')
@@ -49,5 +50,5 @@ def saveuser_view(request):
         contactusform.phone_no = phone_no
         contactusform.description = description
         contactusform.save()
-
+        
     return JsonResponse({"response":"success"})
