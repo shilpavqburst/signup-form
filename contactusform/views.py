5c34e96bb0ed3ef6cd08e381a44f64cffbbf0680
Athul Mohan <athulmohan@Riafys-MacBook-Pro.local>
2021-01-13 12:18:17 +0530

    don

--- contactusform/views.py
+++ contactusform/views.py
@@ -11,17 +11,17 @@ from . import forms
 def contactusform_view(request):
 
     if request.method=="POST":
-        contactusform=Contactusform()
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        phone_no = request.POST.get('phone_no')
-        description=request.POST.get('description')
+        # contactusform=Contactusform()
+        # name = request.POST.get('name')
+        # email = request.POST.get('email')
+        # phone_no = request.POST.get('phone_no')
+        # description=request.POST.get('description')
 
-        contactusform.name =  name
-        contactusform.email = email
-        contactusform.phone_no = phone_no
-        contactusform.description = description
-        contactusform.save()
+        # contactusform.name =  name
+        # contactusform.email = email
+        # contactusform.phone_no = phone_no
+        # contactusform.description = description
+        # contactusform.save()
 
     form = forms.Contactform()
     return render(request,'contactusform/form.html', {'form': form})
