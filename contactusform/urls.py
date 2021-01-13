881368eb0f8a4c55b4120060ea2c8281565c9f53
Athul Mohan <athulmohan@Riafys-MacBook-Pro.local>
2021-01-13 10:56:41 +0530

    repath

--- contactusform/urls.py
+++ contactusform/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-re_path('', RedirectView.as_view(url='/register')),
+
     path('register',views.contactusform_view,name="form"),
+    re_path('', RedirectView.as_view(url='/register')),
     path('database',views.database_view,name="database"),
    
 ]
